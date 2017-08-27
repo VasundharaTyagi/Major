2dc33cb1c43b24d3d48b64f90ad6a55da53643e7
Vasundhara Tyagi <31380877+VasundharaTyagi@users.noreply.github.com>
2017-08-27 20:06:35 +0530

    Update Communities.java

--- app/src/main/java/com/example/vasu/demo/Communities.java
+++ app/src/main/java/com/example/vasu/demo/Communities.java
@@ -25,8 +25,7 @@ public class Communities extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
+        
         return inflater.inflate(R.layout.fragment_communities, container, false);
     }
 
@@ -34,7 +33,7 @@ public class Communities extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //you can set the title for your toolbar here for different fragments different titles
+        
         getActivity().setTitle("Communities");
         //Intent intent = new Intent(this, CommunityList.class);
         //EditText editText = (EditText) findViewById(R.id.edit_message);

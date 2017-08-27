b473caf44cecb6f1e9037b70d9bb77af69636f76
Vasundhara Tyagi <31380877+VasundharaTyagi@users.noreply.github.com>
2017-08-27 20:10:02 +0530

    Update splash.java

--- app/src/main/java/com/example/vasu/demo/splash.java
+++ app/src/main/java/com/example/vasu/demo/splash.java
@@ -9,7 +9,6 @@ import android.os.Handler;
 
 public class splash extends AppCompatActivity {
 
-    // Splash screen timer
     private static int SPLASH_TIME_OUT = 3000;
 
     @Override
@@ -19,19 +18,12 @@ public class splash extends AppCompatActivity {
 
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
+                
                 Intent i = new Intent(splash.this, index.class);
                 startActivity(i);
 
-                // close this activity
                 finish();
             }
         }, SPLASH_TIME_OUT);

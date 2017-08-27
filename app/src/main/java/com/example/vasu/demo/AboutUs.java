08217fdcb1129e78013b4a7f673fdd0f6fe31bea
Vasundhara Tyagi <31380877+VasundharaTyagi@users.noreply.github.com>
2017-08-27 20:04:37 +0530

    Update AboutUs.java

--- app/src/main/java/com/example/vasu/demo/AboutUs.java
+++ app/src/main/java/com/example/vasu/demo/AboutUs.java
@@ -13,8 +13,7 @@ public class AboutUs extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
+        
         return inflater.inflate(R.layout.fragment_aboutus, container, false);
     }
 
@@ -22,7 +21,7 @@ public class AboutUs extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //you can set the title for your toolbar here for different fragments different titles
+        
         getActivity().setTitle("About Us");
     }
 }
